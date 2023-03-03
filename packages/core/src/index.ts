bebfb93133849a4b1c7d7d37b60fe4faf63a2fa6
Avinash <avinash.kumar@renegadeinsurance.com>
2023-03-04 00:18:45 +0530

    feat: inital module setup

---
+++ packages/core/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Prototype
+ *
+ * 1. create ec2 instance
+ * 2. pull code
+ * 3. start docker, build docker images, run docker container
+ * 4. clean docker images
+ * 5. clean ec2 instance
+ */
+
+console.log("hi");
+
+export const a = {};
