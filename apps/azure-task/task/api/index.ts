82261ba651cd8620ab69faedd75e979ee0d2e3a1
Avinash <avinash.kumar@renegadeinsurance.com>
2023-04-14 23:11:37 +0530

    feat: get all threads

--- apps/azure-task/task/api/index.ts
+++ apps/azure-task/task/api/index.ts
@@ -1,15 +1,19 @@
 import { WebApi, getPersonalAccessTokenHandler } from "azure-devops-node-api";
-import { BuildContext, buildContext } from "../buildContext";
-export class ApiClient {
-  constructor(private connection: WebApi, private buildCtx: BuildContext) {}
+import { BuildContextType, buildContext } from "../buildContext";
+import { GitPullRequestCommentThread } from "azure-devops-node-api/interfaces/GitInterfaces";
 
-  static initializeApi() {
-    if (!buildContext?.token) throw new Error("Token now found");
+export type Threads = GitPullRequestCommentThread;
+export class ApiClient {
+  constructor(private connection: WebApi, private buildCtx: BuildContextType) {}
+
+  static async initializeApi() {
+    if (!buildContext?.token) throw new Error("Token not found");
 
     const authHandler = getPersonalAccessTokenHandler(
       buildContext.token as string
     );
-    const connection = new WebApi(buildContext.orgUrl, authHandler);
+    const connection = new WebApi(buildContext.orgUrl, authHandler, {});
+    await connection.connect();
     return new ApiClient(connection, buildContext);
   }
 
@@ -20,7 +24,39 @@ export class ApiClient {
       const api = await this.connection.getGitApi();
       return await api.getPullRequestLabels(repoId, prId);
     } catch (error) {
-      throw new Error("Error: failed to get labels", { cause: error });
+      throw new Error("API-ERROR: failed to get labels", { cause: error });
+    }
+  }
+
+  async getAllThreads() {
+    try {
+      const api = await this.connection.getGitApi();
+      return await api
+        .getThreads(buildContext.repoId, Number(buildContext.prId))
+        .then((res) => {
+          return res
+            .filter(
+              (thread) =>
+                !thread.comments?.[0].author?.displayName?.includes(
+                  "Microsoft.VisualStudio.Services"
+                )
+            )
+            .filter((comment) => !comment?.isDeleted);
+        })
+        .catch((e) => {
+          throw e;
+        });
+    } catch (error) {
+      throw new Error("API-ERROR: failed to get thread", { cause: error });
+    }
+  }
+
+  async updateThreads() {
+    try {
+      const api = await this.connection.getGitApi();
+      return;
+    } catch (error) {
+      throw new Error("API-ERROR: failed to update thread");
     }
   }
 }
