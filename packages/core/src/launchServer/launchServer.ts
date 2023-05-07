7eb1e88ea18bff6c11d546eba2bdbaad7f3116ec
Avinash <avinash.kumar@renegadeinsurance.com>
2023-05-07 13:08:14 +0530

    fix: added dummy buildContext for testing

--- packages/core/src/launchServer/launchServer.ts
+++ packages/core/src/launchServer/launchServer.ts
@@ -1,9 +1,40 @@
 import { $, execa } from "execa";
-import { createReadStream, createWriteStream, existsSync } from "fs";
+import { existsSync } from "fs";
 import { Instance } from "../instance/index.js";
 import { polling, sleep } from "../instance/utils.js";
 import { env } from "../utils/env.js";
-import { buildContext } from "../../buildContext.js";
+import { join } from "path";
+
+//
+const isDev = env.isDev;
+
+const tl = {
+  getVariable: (name: string) => name,
+};
+
+export const buildContext = {
+  repoId:
+    tl.getVariable("Build.Repository.ID") ||
+    "a187c009-7402-429e-9111-a7791e589bed",
+  prId: tl.getVariable("System.PullRequest.PullRequestId") || 46,
+  token: env.pat || tl.getVariable("System.AccessToken"),
+  orgUrl:
+    tl.getVariable("System.CollectionUri") ||
+    "https://dev.azure.com/9958703925dad",
+  repoUrl:
+    tl.getVariable("Build.Repository.URI") ||
+    "https://9958703925dad@dev.azure.com/9958703925dad/bookshelf/_git/Next-docker",
+  projectName: tl.getVariable("System.TeamProject") || "bookshelf",
+  buildReason: tl.getVariable("Build.Reason") || "PullRequest",
+  targetBranch: tl.getVariable("System.PullRequest.targetBranchName") || "",
+  sourceBranch: tl.getVariable("System.PullRequest.SourceBranch") || "develop",
+  clonePath: isDev
+    ? join(process.cwd(), "../.dist/temp")
+    : tl.getVariable("Agent.TempDirectory"),
+  buildDirectory: tl.getVariable("Agent.BuildDirectory") || "",
+} as const;
+
+export type BuildContextType = typeof buildContext;
 
 const config = {
   serverAppPath: "/etc/prbranch/app/app.tar.gz",
