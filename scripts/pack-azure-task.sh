174aa11a0d0fb7ffbd509a260dc57b7d68517e9f
Avinash <avinash.kumar@renegadeinsurance.com>
2023-06-13 11:56:09 +0530

    wip

--- scripts/pack-azure-task.sh
+++ scripts/pack-azure-task.sh
@@ -15,9 +15,10 @@ azure_task_dir="$base_repo_path/apps/azure-task"
 if [[ $PWD == "/Users/avinash/oss/mono-review/apps/azure-task" ]]; then
 azure_task_dir="/Users/avinash/oss/mono-review/apps/azure-task"
 upload_script_path="/Users/avinash/oss/mono-review/uploadScript"
-
 fi
 
+
+chmod -R 777 "$azure_task_dir/.dist"
 
 taskPath="$azure_task_dir/taskVersions/task.$1.json"
 distPath="$azure_task_dir/.dist/task"
@@ -26,7 +27,7 @@ echo "Copying task to $distPath"
 cp -r "$azure_task_dir/vss-extension.json" "$azure_task_dir/.dist"
 cp -r node_modules package.json $upload_script_path "$azure_task_dir/images" "$distPath"
 
-chmod -R +x $PWD
+
 
 
 echo "Creating task.json"
@@ -37,7 +38,18 @@ echo "Installing dependencies"
     cd "$distPath" && pnpm i && cd -
 )
 
-echo "Building extension"
-(
-    cd "$azure_task_dir/.dist" && npx tfx-cli extension create --manifest-globs
-)
+# testing
+    echo "currentWorking dir $PWD"
+
+    # echo "testing" 
+    # (
+    #     ls -al "$azure_task_dir/.dist"
+    # )
+# 
+chmod -R 777 "$azure_task_dir/.dist"
+
+echo "azure_task_dir: $azure_task_dir"
+ls -la "$azure_task_dir/.dist"
+ls -la "$azure_task_dir/.dist/vss-extension.json"
+
+npx tfx-cli extension create --root "$azure_task_dir/.dist" --manifests "$azure_task_dir/.dist/vss-extension.json" --output-path "$azure_task_dir/.dist"
